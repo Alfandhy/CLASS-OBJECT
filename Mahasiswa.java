5f3a964b834817d057d4682668446fe65c4c3913
Alfandhy <43575575+Alfandhy@users.noreply.github.com>
2018-10-09 07:30:46 -0700

    Create Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,50 @@
+public class Mahasiswa {
+	//ini loh atributnya
+	private String nama, nim, alamat, golongandarah, status ;
+	private double tinggibadan, beratbadan ;
+	
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public String getNim() {
+		return nim;
+	}
+	public void setNim(String nim) {
+		this.nim = nim;
+	}
+	public String getAlamat() {
+		return alamat;
+	}
+	public void setAlamat(String alamat) {
+		this.alamat = alamat;
+	}
+	public String getGolongandarah() {
+		return golongandarah;
+	}
+	public void setGolongandarah(String golongandarah) {
+		this.golongandarah = golongandarah;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public double getTinggibadan() {
+		return tinggibadan;
+	}
+	public void setTinggibadan(double tinggibadan) {
+		this.tinggibadan = tinggibadan;
+	}
+	public double getBeratbadan() {
+		return beratbadan;
+	}
+	public void setBeratbadan(double beratbadan) {
+		this.beratbadan = beratbadan;
+	} 
+	
+	}
+
